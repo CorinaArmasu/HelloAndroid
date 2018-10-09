2e4fa070d761b8281c20370dbc178c2c50e984b4
naveenjoshii <kondetinaveen1113@gmail.com>
2018-10-09 19:44:42 +0530

    Displays the user Name when a button gets clicked

--- app/src/main/java/com/example/android/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.android.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+	private EditText eText;
+	private Button button;
+	private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+	eText = (EditText)findViewById(R.id.et);
+	textView = (TextView)findViewById(R.id.tv);
+	button =(Button)findViewById(R.id.btn);
+	String s = eText.getText().toString();
+	button.setOnClickListener(new View.OnClickListener {
+	public void OnClick(View v){
+	textView.setText("Hello Welcome "+s);
+
+	}
+	});
+
     }
 }
